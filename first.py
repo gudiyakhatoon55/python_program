9318638ef00ee65039b0fa9de146dab0f0ff40e2
gudiyakhatoon55 <gudiyakhatoonhtce55@gmail.com>
2026-02-04 19:32:28 +0530

    my is my progra

---
+++ first.py
@@ -0,0 +1,9 @@
+Name="Gudiya_Khatoon"
+Qualification="12th_pass"
+Address="Ujranarayanpur"
+Age=18
+Location="Mirganj"
+State="Bihar"
+Country="India"
+
+print(f"Hi, I am {Name}, I am a {Qualification}, I live in {Address}, I am {Age} years old, My location is {Location}, I am from {State}, I am from {Country}")
